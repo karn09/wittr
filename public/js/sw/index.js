8fa7aecdf6e3a365b23f53832ff3605b81225076
John Nieves <nievesj@nievesj-macbookpro2.roam.corp.google.com>
2016-05-26 12:41:21 -0400

    fetch gif

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,13 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+    // new Response('<h1 class="a-winner-is-me">Hello World<h1>', {
+    //   "headers": {
+    //     "Content-Type": "text/html",
+    //     "foo": "bar"
+    //   }
+    // })
+    fetch('/imgs/dr-evil.gif')
+  );
 });
